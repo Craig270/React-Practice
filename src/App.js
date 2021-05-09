63ebdf9bbbc1a149380a465c7072a4f45522d7dc
Craig Neff <ifly12610@gmail.com>
2021-05-08 23:16:12 -0600

    practicing more with React. Going back to the basics to make sure I can do them well

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleClick() {
+    console.log("I'm working!");
+  }
+
+  render() {
+    return (
+      <div className="btn btn-primary">
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
